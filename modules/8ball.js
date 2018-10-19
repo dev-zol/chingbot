be8bf697ff9302d06351dbeb951fe3ebcfef727a
dev-zol <tkrobotics@gmail.com>
2018-10-19 09:30:06 -0500

    added chingball

---
+++ modules/8ball.js
@@ -0,0 +1,44 @@
+module.exports = {
+    name: 'chingball',
+    description: 'chingbot will search your soul',
+    execute(message, args) {
+        const questions = [
+            'does',
+            'will',
+            'am',
+            'is',
+            'did',
+            'do',
+            'can',
+            'as',
+            'are'
+        ];
+
+        const answers = [
+            'Sure i guess',
+            'i mean why not',
+            'sur',
+            'Yes definitely',
+            'You may rely on it',
+            'As I see it, yes',
+            'probs',
+            'Outlook good',
+            'Yes',
+            'Ching says yes',
+            'Don\'t count on it',
+            'can you don\'t',
+            'yah, no',
+            'don\'t do it',
+            'definitely.not',
+            'no, kys yourself'
+        ];
+        const chance = parseInt(Math.random() * 16);
+
+        for(const question in questions) {
+            console.log(question)
+            if(args[0] === questions[question]) {
+                message.channel.send(answers[chance]);
+            }
+        }
+    }
+}
